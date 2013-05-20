f098b512ac6505f8f279a6db7cbd499a47068716
Lukasz <Lukasz@10.20.125.73>
2013-05-20 08:52:23 +0200

    Start

---
+++ src/rozproszone/Rozproszone.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rozproszone;
+
+/**
+ *
+ * @author Lukasz
+ */
+public class Rozproszone {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
