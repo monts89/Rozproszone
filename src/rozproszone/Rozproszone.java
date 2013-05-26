c378dc57d269f79398086edbbdff781a98213f33
Lukasz <Lukasz@10.20.125.73>
2013-05-26 23:44:24 +0200

    GUI pozwalające przeglądać przestrzeń.
    W boxie wybieramy głębokość i mamy podgląd tej warstwy.
    
    W window mamy metodę setCellSpace. Powoduje ona zamiane istniejącej przestrzeni oraz automatyczne przerysowaniei. Najlepiej chyba korzystac tylko z niej.

--- src/rozproszone/Rozproszone.java
+++ src/rozproszone/Rozproszone.java
@@ -8,19 +8,41 @@ import ca.CellSpace;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import server.ServerMain;
+import visualization.MyWindow;
 
 /**
  *
  * @author Lukasz
  */
 public class Rozproszone {
-
+    private static MyWindow window = new MyWindow();
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         ServerMain main = new ServerMain(new CellSpace(10, 10, 1));
-
+        
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                window.setVisible(true);
+            }
+        });
+        
+        //Tests/////////////////////////////////////////////////////
+        CellSpace newSpace = new CellSpace(100, 100, 100);
+        newSpace.setValue(30, 30, 0, 100);
+        newSpace.setValue(50, 50, 0, 100);
+        newSpace.setValue(20, 40, 0, 100);
+        newSpace.setValue(90, 70, 0, 100);
+        
+        newSpace.setValue(90, 30, 1, 100);
+        newSpace.setValue(90, 50, 1, 100);
+        newSpace.setValue(90, 40, 1, 100);
+        newSpace.setValue(90, 70, 1, 100);
+        
+        window.setCellSpace(newSpace);
+        /////////////////////////////////////
+        
         main.writeSpace();
         try {
             main.makeRemoteCall(args);
